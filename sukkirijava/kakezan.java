481de9a2aacc9f8962983d0e6c627d2992e96e06
mores1 <97749252+mores1@users.noreply.github.com>
2022-05-02 23:59:00 +0900

    fe

---
+++ sukkirijava/kakezan.java
@@ -0,0 +1,14 @@
+class test {
+
+  public static void main(String[] args) {
+    int i = [0,1,2,3,4,5,6,7,8,9]
+    int cnt = 0;
+
+    for (int i = 1; i <= 1000000; i++) {
+      
+      if (n = 4)
+      cnt ++;
+    }
+    System.out.println("");
+  }
+}
